b58a7f103ca4d046813d0ee0007175c3358a4efe
MerePT <1466005267@qq.com>
2022-01-15 14:31:34 +0800

    V0.3.2 优化电源使用情况

--- ResultWindowWithNames.xaml.cs
+++ ResultWindowWithNames.xaml.cs
@@ -13,20 +13,18 @@ namespace GradePointAverageCalulatorForSWPU {
             InitializeComponent();
             GradePointAverage = GPA;
             Results.ItemsSource = GradePointAverage.GradesAndPoints;
-            var timer = new DispatcherTimer();
-            timer.Tick += TimerTick;
-            timer.IsEnabled = true;
-        }
-
-        void TimerTick(object sender, EventArgs e) => ResultOfGpa.Content = $"总修读学分: {GradePointAverage.TotalPoint} " +
+            ResultOfGpa.Content = $"总修读学分: {GradePointAverage.TotalPoint} " +
                 $"已通过学分: {GradePointAverage.TotalNotFailedPoint} " +
                 $"不及格科目数: {GradePointAverage.Fails} " +
                 $"平均学分绩点: {GradePointAverage.Result:0.00}";
+        }
 
         private void MenuItem_Click(object sender, RoutedEventArgs e) {
-            var index = Results.SelectedIndex;
-            new ChangeData(index, GradePointAverage.GradesAndPoints, GradePointAverage).Show();
-            
+            new ChangeData(Results.SelectedIndex, GradePointAverage.GradesAndPoints, GradePointAverage).ShowDialog();
+            ResultOfGpa.Content = $"总修读学分: {GradePointAverage.TotalPoint} " +
+                $"已通过学分: {GradePointAverage.TotalNotFailedPoint} " +
+                $"不及格科目数: {GradePointAverage.Fails} " +
+                $"平均学分绩点: {GradePointAverage.Result:0.00}";
         }
     }
 }
